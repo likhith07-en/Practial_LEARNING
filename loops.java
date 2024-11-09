52964b57775ba22a1d6519454f798cab405a647c
Mohan <51405902+Mk0699@users.noreply.github.com>
2024-11-09 17:20:40 +0530

    i removed some syntax errors 👍

--- loops.java
+++ loops.java
@@ -7,12 +7,11 @@ class loops
              System.out.println("Enter the parameters");
              int i=sc.nextInt();
              String k="l";
-             for(int a=0;a==i;a++)
+             for(int a=0;a<=i;a++)
                 { 
                  for(int b=0;b<=a;b++)
                   {
                     System.out.print(k+"\t");
-                    ;
                   }
                  System.out.println("  ");
                  }
